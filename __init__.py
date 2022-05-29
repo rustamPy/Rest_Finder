956780f4ce0b4d2f4e65373491f648ff5ec167c8
rustamPy <kerimov.rustam@live.ru>
2022-05-29 16:16:59 +0200

    Working

--- __init__.py
+++ __init__.py
@@ -4,19 +4,19 @@ import pymongo
 
 
 class mongo_connection:
-    conn = None
+    col = None
 
     def connect(self):
         myclient = pymongo.MongoClient("localhost", 27017)
         mydb = myclient["restdb"]
-        self.conn = mydb["restaurants"]
+        self.col = mydb["restaurants"]
 
     def query(self, sql):
-        cursor = self.conn.find(sql)
+        cursor = self.col.find(sql)
         return cursor
 
     def setrest(self, val):
-        self.conn.insert_one(val)
+        self.col.insert_one(val)
 
 
 db = mongo_connection()
@@ -48,21 +48,23 @@ def getrestaurants():
 
     geolocator = Nominatim(user_agent='myapplication')
     location = geolocator.geocode(final_addr)
+
     lat = float(location.raw['lat'])
     lon = float(location.raw['lon'])
     nearby_restaurants = [{'orig_lat': lat, 'orig_lon': lon}]
-
-    print(lat, lon)
+    print(nearby_restaurants)
+    print(lon, lat)
 
     METERS_PER_MILE = 1609.34
 
     filters = {'location': {'$nearSphere': {'$geometry': {'type': "Point",
                                                           'coordinates': [float(lon), float(lat)]},
-                                            '$maxDistance': int(rad) * METERS_PER_MILE}},
+                                            '$maxDistance': METERS_PER_MILE*int(rad)}},
                'name': {'$regex': restname, "$options": "i"}}
-    print(type(filters))
+    #
+    print(filters)
     cursor = db.query(filters)
-
+    print(cursor)
     for cur in cursor:
         print(cur)
         nearby_restaurants.append({
@@ -71,7 +73,7 @@ def getrestaurants():
             'lon': cur['location']['coordinates'][0]
         })
     # nearby_restaurants=[{'orig_lat': 40.3754434, 'orig_lon': 49.8326748}, {'restaurant_name': 'Derya Fish House', 'lat': 40.304027636182674, 'lon': 49.827603950210836}]
-    print(nearby_restaurants)
+    print(f'nearby rests --> {nearby_restaurants}')
     return jsonify(nearby_restaurants)
 
 
